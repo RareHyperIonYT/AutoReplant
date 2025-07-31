4d286079094de83364e5ca25c59624dfe4ead244
RareHyperIon <rarehyperionyt@gmail.com>
2025-07-31 23:01:28 +0100

    Initial Commit

---
+++ src/main/java/me/rarehyperion/autoreplant/AutoReplant.java
@@ -0,0 +1,17 @@
+package me.rarehyperion.autoreplant;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AutoReplant extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
